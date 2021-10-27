11edf20e376964b4568b9b7815c202cb0e9a0536
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-10-27 14:43:15 -0400

    added test specs for ForgotPassword

--- alchemyFront/src/app/forgot-password/forgot-password.component.spec.ts
+++ alchemyFront/src/app/forgot-password/forgot-password.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AuthenticationService } from '../services/authentication.service';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
 describe('ForgotPasswordComponent', () => {
   let component: ForgotPasswordComponent;
   let fixture: ComponentFixture<ForgotPasswordComponent>;
+  let authService: AuthenticationService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent ]
+  class MockService {
+    ForgotPassword(email:string){}
+  }
+
+  beforeEach( () => {
+    TestBed.configureTestingModule({
+      declarations: [ ForgotPasswordComponent ],
+      providers:[{provide: AuthenticationService, useClass: MockService}]
     })
     .compileComponents();
   });
@@ -17,9 +25,32 @@ describe('ForgotPasswordComponent', () => {
     fixture = TestBed.createComponent(ForgotPasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    authService = TestBed.inject(AuthenticationService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call the ForgotPassword() method', () => {
+    let btn = fixture.debugElement.query(By.css(".btn")).nativeElement;
+    btn.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.authService.ForgotPassword).toHaveBeenCalled();
+    })
+  });
+  it('should display alert when ForgotPassword() succeeds', () => {
+    let authServiceSpy= spyOn(authService, 'ForgotPassword');
+    let alertSpy = spyOn(window, 'alert');
+    let btn = fixture.debugElement.query(By.css(".btn")).nativeElement;
+    btn.click();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(window.alert).toHaveBeenCalledWith('message');
+    })
+  });
+
 });
