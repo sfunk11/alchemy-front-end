6a9c5168187da67d02ee411186bc951fed60b91a
Aiuner <demonspawndaemnyxea@gmail.com>
2021-11-02 16:45:45 -0400

    Pushing this up just for the sake of it... profile is not working at the moment. Some CSS has been tweaked/altered but changes may not be inherently obvious.

--- alchemyFront/src/app/app-routing.module.ts
+++ alchemyFront/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 import { BoardComponent } from './board/board.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { AuthGuard } from './services/guard/auth.guard';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: "sign-in", component: SignInComponent},
   { path: 'register-user', component: SignUpComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate:[AuthGuard] },
   { path: 'board', component: BoardComponent, canActivate:[AuthGuard] },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
