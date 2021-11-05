f7d05c37c33f1939d51e92ab367c20bcd188ff9f
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-11-05 14:46:16 -0400

    committing current progress

--- alchemyFront/src/app/puzzle-selector/puzzle-selector.component.ts
+++ alchemyFront/src/app/puzzle-selector/puzzle-selector.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api/api.service';
+import { AuthenticationService } from '../services/auth/authentication.service';
+import { Photo } from '../services/util/photo';
 
 @Component({
   selector: 'app-puzzle-selector',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./puzzle-selector.component.css']
 })
 export class PuzzleSelectorComponent implements OnInit {
+  puzzleList = [] as Photo[];
 
-  constructor() { }
+  constructor(private api:ApiService, private auth:AuthenticationService) { }
 
   ngOnInit(): void {
+    this.loadPuzzleList();
+  }
+
+  loadPuzzleList() {
+    this.api.getAllPuzzles().subscribe(
+      result => {
+        console.log(result);
+        for(let i=0; i<result.length; i++){
+          if (result[i].isApproved){
+            this.puzzleList.push(result[i])
+          }else if (result[i].uploader.email == this.auth.userData.email){
+            this.puzzleList.push(result[i])
+          }
+        }
+        console.log(this.puzzleList);
+      }
+    )
   }
 
 }
